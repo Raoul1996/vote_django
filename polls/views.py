37aae36b591a65eeb4b77e53d8f3ab55ddebf30f
Raoul1996 <baobaozhou199606@gmail.com>
2018-04-05 00:34:53 +0800

    fix: show the future vote which we sholdn't showing

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -15,7 +16,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the latest five published question"""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
